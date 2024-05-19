149e1c63c722390175aa72c291c1b56b5e155f9b
j-miniee <potatopizza0518@naver.com>
2024-05-19 10:20:53 +0900

    24-05-19

--- week9/Lab_quickSort/quickSort.c
+++ week9/Lab_quickSort/quickSort.c
@@ -3,42 +3,37 @@
 #include<stdlib.h>
 #include<time.h>
 
+void swap(int* arr, int i, int j) {
+	int  temp;
+
+	temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+}
+
 int partition(int A[], int p, int r) {
 	int i, j;
 	int pivot;
 	int t;
 
 	i = p - 1; //시작 바로 왼쪽
-	j = p; //loop 변수임
-
-	/*
-	t = rendom(...) //random int from (p, r)
-	swap A[t] <-> A[r]
-	pivot = A[r];
-	*/
 
 	pivot = A[r];
 
-	for (j = p; j < r; j++) {
-		if (A[j] < pivot) {
+	for (j = p; j < r; j++) { 
+		if (A[j] < pivot) { 
 			i++; 
-			//swap A[i] <-> A[j]
-			t = A[i];
-			A[i] = A[j];
-			A[j] = t;
+			swap(A, i, j);
 		}
 	}
 	i++;
-	//swap A[i] <-> A[r]
-	t = A[i];
-	A[i] = A[r];
-	A[r] = t;
+	swap(A, i, j);
 
 	return i; //pivot의 위치를 반환함
 }
 
 int quickSort(int *A, int p, int r) {
-	if (p > r) {
+	if (p < r) {
 		int q = partition(A, p, r);
 		quickSort(A, p, q - 1);
 		quickSort(A, q + 1, r);
@@ -47,6 +42,7 @@ int quickSort(int *A, int p, int r) {
 
 void printList(int A[], int s, int e) {
 	int i;
+
 	for (i = s; i < e; i++) {
 		printf("%d ", A[i]);
 	}
@@ -61,13 +57,14 @@ int main() {
 	arr = (int*)malloc(sizeof(int) * n);
 	
 	srand(time(NULL));
-	for (i = 0; i < n; i++)
+
+	for (i = 0; i < n; i++) 
 		arr[i] = rand() % 100;
 
 	printList(arr, 0, n);
 	quickSort(arr, 0, n - 1);
 	printList(arr, 0, n);
-
+	
 	free(arr);
 
 	return 0;
